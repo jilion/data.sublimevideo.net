3b97a25d7091c1bca2aadc53f96a99f3d073987f
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2011-06-21 15:07:21 +0200

    Tried to remove 'em-synchrony/mongoid'

--- app/controllers/player_views_controller.rb
+++ app/controllers/player_views_controller.rb
@@ -1,7 +1,7 @@
 class PlayerViewsController < Goliath::API
 
   def response(env)
-    # SiteUsage.increment(params[:site_token], :player_views)
+    SiteUsage.increment(params[:site_token], :player_views)
 
     [200, { 'Content-Type' => 'text/javascript' }, 'sublimevideo.pInc=true']
   end
