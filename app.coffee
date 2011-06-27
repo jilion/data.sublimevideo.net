8d07e3e3677142321396449e2fdd751bc72fcb23
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2011-06-27 15:14:58 +0200

    Added Mongolian

--- app.coffee
+++ app.coffee
@@ -1,7 +1,23 @@
-express = require('express')
-app     = express.createServer express.logger()
+express   = require 'express'
+app       = express.createServer express.logger()
 
-app.get '/', (request, response) ->
-  response.send 'Hi!'
+Mongolian = require 'mongolian'
+server = new Mongolian 'swan.mongohq.com:27021'
+db     = server.db 'app182505'
+db.auth 'heroku', 'vnfv66001682rqdvzd4k9k'
+usages = db.collection 'site_usages_test'
+
+today = () ->
+  date  = new Date
+  milli = Date.UTC date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate()
+  new Date(milli)
+
+app.get '/', (req, res) ->
+  res.send 'Hi!'
+
+app.get '/p/:token', (req, res) ->
+  usages.update { 't' : req.params.token, 'd' : today() }, { '$inc': { 'p': 1 } }, true
+  res.header "Content-Type", "text/javascript"
+  res.send 'sublimeVideo.pInc=true'
 
 app.listen process.env.PORT || 3000
--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port, function(){
-  console.log("Listening on " + port);
-});
+(function() {
+  var Mongolian, app, db, express, server, today, usages;
+  express = require('express');
+  app = express.createServer(express.logger());
+  Mongolian = require('mongolian');
+  server = new Mongolian('swan.mongohq.com:27021');
+  db = server.db('app182505');
+  db.auth('heroku', 'vnfv66001682rqdvzd4k9k');
+  usages = db.collection('site_usages_test');
+  today = function() {
+    var date, milli;
+    date = new Date;
+    milli = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
+    return new Date(milli);
+  };
+  app.get('/', function(req, res) {
+    return res.send('Hi!');
+  });
+  app.get('/p/:token', function(req, res) {
+    usages.update({
+      't': req.params.token,
+      'd': today()
+    }, {
+      '$inc': {
+        'p': 1
+      }
+    }, true);
+    res.header("Content-Type", "text/javascript");
+    return res.send('sublimeVideo.pInc=true');
+  });
+  app.listen(process.env.PORT || 3000);
+}).call(this);
