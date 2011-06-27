d36dae49013d3aa8304108e866261d3cd8199fcc
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2011-06-27 10:14:04 +0200

    CP 25 -> 50

--- config/redis.rb
+++ config/redis.rb
@@ -1,3 +1,3 @@
-config['redis'] = EM::Synchrony::ConnectionPool.new(size: 25) do
+config['redis'] = EM::Synchrony::ConnectionPool.new(size: 50) do
   Redis.connect(url: ENV['REDISTOGO_URL'])  # localhost if ENV['REDISTOGO_URL'] isn't present
 end
