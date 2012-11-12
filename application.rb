cb4a8f969cd4cebcbcba1fc4d61767f80a18bc63
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-11-12 13:38:51 +0100

    Improve OPTIONS

--- application.rb
+++ application.rb
@@ -1,15 +1,8 @@
 #!/usr/bin/env ruby
 require 'goliath'
 require 'yajl'
-# require 'rack/cors'
 
 class Application < Goliath::API
-  # use Rack::Cors do
-  #   allow do
-  #     origins '*'
-  #     resource '/*', headers: :any, methods: :get
-  #   end
-  # end
   use Goliath::Rack::Params                 # parse & merge query and body parameters
   use Goliath::Rack::DefaultMimeType        # cleanup accepted media types
   use Goliath::Rack::Formatters::JSON       # JSON output formatter
@@ -19,10 +12,16 @@ class Application < Goliath::API
   def response(env)
     case env['REQUEST_METHOD']
     when "OPTIONS"
-      [200, { "Access-Control-Allow-Origin" => '*', 'Access-Control-Allow-Headers' => 'Content-Type' }, {}]
+      [200, {
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Allow-Methods' => 'POST',
+        'Access-Control-Allow-Headers' => 'Content-Type',
+        'Access-Control-Allow-Credentials' => true,
+        'Access-Control-Max-Age' => 60 # 1728000
+      }, {}]
     else
       response_params = Yajl::Encoder.encode(params)
-      [200, {}, response_params]
+      [200, { "Access-Control-Allow-Origin" => '*' }, response_params]
     end
   end
 end
