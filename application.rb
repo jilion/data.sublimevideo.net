d6a001485694290490141bd3655bcaedca2abccd
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-11-12 12:46:14 +0100

    Some puts

--- application.rb
+++ application.rb
@@ -17,6 +17,10 @@ class Application < Goliath::API
   use Goliath::Rack::Heartbeat              # respond to /status with 200, OK (monitoring, etc)
 
   def response(env)
+    puts env['REQUEST_PATH']
+    puts env['REQUEST_METHOD']
+    puts params
+
     body = Yajl::Encoder.encode(status: 'ok')
     [200, { "Access-Control-Allow-Origin" => '*', 'Access-Control-Allow-Headers' => 'Content-Type' }, body]
   end
