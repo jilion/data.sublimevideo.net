fd977a7cfe70e0a7623f74d6add12b5846424616
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-11-12 12:20:07 +0100

    Test with rack/cors in top

--- application.rb
+++ application.rb
@@ -4,20 +4,20 @@ require 'yajl'
 require 'rack/cors'
 
 class Application < Goliath::API
-  use Goliath::Rack::Params                 # parse & merge query and body parameters
-  use Goliath::Rack::DefaultMimeType        # cleanup accepted media types
-  use Goliath::Rack::Formatters::JSON       # JSON output formatter
-  use Goliath::Rack::Render                 # auto-negotiate response format
-  use Goliath::Rack::Heartbeat              # respond to /status with 200, OK (monitoring, etc)
   use Rack::Cors do
     allow do
       origins '*'
       resource '/*', headers: :any, methods: :get
     end
   end
+  use Goliath::Rack::Params                 # parse & merge query and body parameters
+  use Goliath::Rack::DefaultMimeType        # cleanup accepted media types
+  use Goliath::Rack::Formatters::JSON       # JSON output formatter
+  use Goliath::Rack::Render                 # auto-negotiate response format
+  use Goliath::Rack::Heartbeat              # respond to /status with 200, OK (monitoring, etc)
 
   def response(env)
     body = Yajl::Encoder.encode(status: 'ok')
-    [200, { "Access-Control-Allow-Origin" => '*' }, body]
+    [200, {}, body]
   end
 end
