6225760d741b0f05ff2254b2e01d5fe737e3b84c
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-03-11 10:23:37 +0100

    Configure Sidekiq

--- config.rb
+++ config.rb
@@ -1,6 +1,9 @@
 require 'sidekiq'
 Sidekiq.configure_client do |config|
-  config.redis = { size: 50 }
+  config.redis = { size: 20 }
+end
+Sidekiq.configure_server do |config|
+  config.redis = { size: 20 }
 end
 
 require 'librato/metrics'
