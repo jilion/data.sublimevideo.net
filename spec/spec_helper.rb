5575cbceedd1103d41cb6bfb38d6a8c96c8eb783
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-03-19 16:05:53 +0100

    Update RSpec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,10 +11,10 @@ require_relative '../config.rb'
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
-  config.filter_run_including focus: true
+  config.filter_run_including focus: ENV['FOCUS'] != 'false'
   config.mock_with :rspec
   config.fail_fast = ENV['FAST_FAIL'] != 'false'
-  config.order = 'random'
+  config.order = ENV['ORDER'] || 'random'
 end
 
 require 'redis'
