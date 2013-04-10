89a249ab9cb18b4f7962e5d8d96761593f42b8a2
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-04-10 08:53:34 +0200

    Send Librato::Metrics each 5 minutes

--- config.rb
+++ config.rb
@@ -3,4 +3,4 @@ Airbrake.configure do |config|
 end
 
 Librato::Metrics.authenticate ENV['LIBRATO_METRICS_USER'], ENV['LIBRATO_METRICS_TOKEN']
-$metrics_queue = Librato::Metrics::Queue.new(autosubmit_interval: 60)
+$metrics_queue = Librato::Metrics::Queue.new(autosubmit_interval: 300)
