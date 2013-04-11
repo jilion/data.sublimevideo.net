d4628438aed0235ad776ceea710b9b654326f9e0
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-04-11 08:37:35 +0200

    Send Honeybadger notice in a thread

--- config.rb
+++ config.rb
@@ -1,5 +1,9 @@
 Honeybadger.configure do |config|
   config.api_key = '7586a6c1'
+
+  config.async do |notice|
+    Thread.new { notice.deliver }
+  end
 end
 
 Librato::Metrics.authenticate ENV['LIBRATO_METRICS_USER'], ENV['LIBRATO_METRICS_TOKEN']
