2720d91138e1f92c644d34a27892c5e54b8f92dc
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-11-13 11:53:32 +0100

    Comment stats creation

--- app/actions/stat_action.rb
+++ app/actions/stat_action.rb
@@ -2,7 +2,7 @@ class StatAction < Cramp::Action
   use_fiber_pool
 
   def start
-    Stat.inc_and_push_stats(params, request.env["HTTP_USER_AGENT"])
+    # Stat.inc_and_push_stats(params, request.env["HTTP_USER_AGENT"])
     # render transparent gif
     render "GIF89a\u0001\u0000\u0001\u0000\x80\xFF\u0000\xFF\xFF\xFF\u0000\u0000\u0000,\u0000\u0000\u0000\u0000\u0001\u0000\u0001\u0000\u0000\u0002\u0002D\u0001\u0000;"
     finish
